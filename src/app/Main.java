c5c033900d5d7df6dc3653bdbcd59940708b90c2
Sojo506 <fsojodev@gmail.com>
2025-07-06 17:28:51 -0600

    call MainView

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,25 @@
 package app;
 
+import view.MainView;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainView().setVisible(true);
+        });
     }
 }
