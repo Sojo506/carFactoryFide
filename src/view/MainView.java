2d8067fbf547b69f68e00ef9b293f4f853599977
Sojo506 <fsojodev@gmail.com>
2025-07-11 17:37:51 -0600

    add and style conveyorBeltPanel

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -8,6 +8,7 @@ public class MainView extends javax.swing.JFrame {
 
     public MainView() {
         player = new Player("Rick Sánchez");
+        setUndecorated(true);
         initComponents();
         setResizable(false);
         setLocationRelativeTo(null);
@@ -78,6 +79,12 @@ public class MainView extends javax.swing.JFrame {
         assemblySubTitle1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         assemblyList1 = new javax.swing.JList<>();
+        conveyorBeltPanel = new javax.swing.JPanel();
+        materialLabel1 = new javax.swing.JButton();
+        materialLabel2 = new javax.swing.JButton();
+        materialLabel3 = new javax.swing.JButton();
+        materialLabel4 = new javax.swing.JButton();
+        materialLabel5 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -622,6 +629,87 @@ public class MainView extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        materialLabel1.setBackground(new java.awt.Color(47, 47, 47));
+        materialLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        materialLabel1.setText("Carrocería especial");
+        materialLabel1.setBorderPainted(false);
+        materialLabel1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        materialLabel1.setFocusPainted(false);
+        materialLabel1.setFocusable(false);
+
+        materialLabel2.setBackground(new java.awt.Color(47, 47, 47));
+        materialLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        materialLabel2.setText("Llantas de trabajo");
+        materialLabel2.setBorderPainted(false);
+        materialLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        materialLabel2.setFocusPainted(false);
+        materialLabel2.setFocusable(false);
+
+        materialLabel3.setBackground(new java.awt.Color(47, 47, 47));
+        materialLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        materialLabel3.setText("Carrocería");
+        materialLabel3.setBorderPainted(false);
+        materialLabel3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        materialLabel3.setFocusPainted(false);
+        materialLabel3.setFocusable(false);
+
+        materialLabel4.setBackground(new java.awt.Color(47, 47, 47));
+        materialLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        materialLabel4.setText("Llantas de trabajo");
+        materialLabel4.setBorderPainted(false);
+        materialLabel4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        materialLabel4.setFocusPainted(false);
+        materialLabel4.setFocusable(false);
+
+        materialLabel5.setBackground(new java.awt.Color(47, 47, 47));
+        materialLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        materialLabel5.setText("Motor deportivo");
+        materialLabel5.setBorderPainted(false);
+        materialLabel5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        materialLabel5.setFocusPainted(false);
+        materialLabel5.setFocusable(false);
+
+        javax.swing.GroupLayout conveyorBeltPanelLayout = new javax.swing.GroupLayout(conveyorBeltPanel);
+        conveyorBeltPanel.setLayout(conveyorBeltPanelLayout);
+        conveyorBeltPanelLayout.setHorizontalGroup(
+            conveyorBeltPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(conveyorBeltPanelLayout.createSequentialGroup()
+                .addGap(145, 145, 145)
+                .addComponent(materialLabel2)
+                .addGap(176, 176, 176)
+                .addComponent(materialLabel1)
+                .addContainerGap(163, Short.MAX_VALUE))
+            .addGroup(conveyorBeltPanelLayout.createSequentialGroup()
+                .addGap(47, 47, 47)
+                .addComponent(materialLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(materialLabel5)
+                .addGap(138, 138, 138)
+                .addComponent(materialLabel4)
+                .addGap(41, 41, 41))
+        );
+        conveyorBeltPanelLayout.setVerticalGroup(
+            conveyorBeltPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(conveyorBeltPanelLayout.createSequentialGroup()
+                .addGroup(conveyorBeltPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, conveyorBeltPanelLayout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(materialLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(conveyorBeltPanelLayout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(materialLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 12, Short.MAX_VALUE)))
+                .addGap(18, 18, 18)
+                .addComponent(materialLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(13, 13, 13))
+            .addGroup(conveyorBeltPanelLayout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(materialLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addComponent(materialLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout contentPanelLayout = new javax.swing.GroupLayout(contentPanel);
         contentPanel.setLayout(contentPanelLayout);
         contentPanelLayout.setHorizontalGroup(
@@ -630,24 +718,31 @@ public class MainView extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(contentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(assemblyPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(hudPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(hudPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(contentPanelLayout.createSequentialGroup()
+                        .addComponent(conveyorBeltPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         contentPanelLayout.setVerticalGroup(
             contentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(contentPanelLayout.createSequentialGroup()
-                .addContainerGap()
+                .addContainerGap(22, Short.MAX_VALUE)
                 .addComponent(hudPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(assemblyPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(203, Short.MAX_VALUE))
+                .addGap(31, 31, 31)
+                .addComponent(conveyorBeltPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(contentPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(contentPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -681,12 +776,18 @@ public class MainView extends javax.swing.JFrame {
     private javax.swing.JLabel assemblyTitle2;
     private javax.swing.JLabel assemblyTitle3;
     private javax.swing.JPanel contentPanel;
+    private javax.swing.JPanel conveyorBeltPanel;
     private javax.swing.JLabel factoryLabel;
     private javax.swing.JLabel goalLabel;
     private javax.swing.JPanel hudPanel;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JButton materialLabel1;
+    private javax.swing.JButton materialLabel2;
+    private javax.swing.JButton materialLabel3;
+    private javax.swing.JButton materialLabel4;
+    private javax.swing.JButton materialLabel5;
     private javax.swing.JLabel moneyLabel;
     private javax.swing.JPanel orderPanel1;
     private javax.swing.JPanel orderPanel2;
