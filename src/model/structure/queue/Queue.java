25bcfe2b26e1e2e229a3176488e02628bd1a92ae
Sojo506 <fsojodev@gmail.com>
2025-07-17 15:00:01 -0600

    add dequeueAt and get methods

--- src/model/structure/queue/Queue.java
+++ src/model/structure/queue/Queue.java
@@ -1,5 +1,7 @@
 package model.structure.queue;
 
+import model.structure.linkedlist.Node;
+
 // Queue.java
 public class Queue<T> {
 
@@ -19,7 +21,7 @@ public class Queue<T> {
             front = newNode;
             rear = newNode;
         } else {
-            rear.next = newNode;
+            rear.setNext(newNode);
             rear = newNode;
         }
         size++;
@@ -30,8 +32,8 @@ public class Queue<T> {
             throw new RuntimeException("Cola vacía");
         }
 
-        T removed = front.data;
-        front = front.next;
+        T removed = front.getData();
+        front = front.getNext();
         size--;
 
         if (front == null) {
@@ -41,12 +43,48 @@ public class Queue<T> {
         return removed;
     }
 
+    public T dequeueAt(int idx) {
+        if (idx < 0 || idx >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (idx == 0) {
+            return dequeue();
+        }
+
+        Node<T> prev = front;
+
+        for (int i = 0; i < idx - 1; i++) {
+            prev = prev.getNext();
+        }
+
+        Node<T> del = prev.getNext();
+        prev.setNext(del.getNext());
+
+        if (del == rear) {
+            rear = prev;
+        }
+        size--;
+        return del.getData();
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> cur = front;
+        for (int i = 0; i < index; i++) {
+            cur = cur.getNext();
+        }
+        return cur.getData();
+    }
+
     public T peek() {
         if (isEmpty()) {
             throw new RuntimeException("Cola vacía");
         }
 
-        return front.data;
+        return front.getData();
     }
 
     public boolean isEmpty() {
@@ -61,9 +99,10 @@ public class Queue<T> {
         Object[] arr = new Object[size];
         Node<T> current = front;
         int i = 0;
+        
         while (current != null) {
-            arr[i++] = current.data;
-            current = current.next;
+            arr[i++] = current.getData();
+            current = current.getNext();
         }
         return arr;
     }
